aabc0e899c956c6bc64285489fc829003e15c790
Tom Maiaroto <tom@shift8creative.com>
2014-06-18 22:39:12 -0500

    removing e2e tests temporarily. no likey front-end testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         command: './node_modules/.bin/bower install'
       },
       font_awesome_fonts: {
-        command: 'cp -R bower_components/components-font-awesome/font app/font'
+        command: 'cp -R bower_components/components-font-awesome/fonts app/font'
       }
     },
 
@@ -77,14 +77,6 @@ module.exports = function(grunt) {
       },
       midway_auto: {
         configFile: './test/karma-midway.conf.js'
-      },
-      e2e: {
-        configFile: './test/karma-e2e.conf.js',
-        autoWatch: false,
-        singleRun: true
-      },
-      e2e_auto: {
-        configFile: './test/karma-e2e.conf.js'
       }
     },
 
@@ -127,16 +119,14 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('test', ['connect:testserver','karma:unit','karma:midway', 'karma:e2e']);
+  grunt.registerTask('test', ['connect:testserver','karma:unit','karma:midway']);
   grunt.registerTask('test:unit', ['karma:unit']);
   grunt.registerTask('test:midway', ['connect:testserver','karma:midway']);
-  grunt.registerTask('test:e2e', ['connect:testserver', 'karma:e2e']);
 
   //keeping these around for legacy use
   grunt.registerTask('autotest', ['autotest:unit']);
   grunt.registerTask('autotest:unit', ['connect:testserver','karma:unit_auto']);
   grunt.registerTask('autotest:midway', ['connect:testserver','karma:midway_auto']);
-  grunt.registerTask('autotest:e2e', ['connect:testserver','karma:e2e_auto']);
 
   //installation-related
   grunt.registerTask('install', ['shell:npm_install','shell:bower_install','shell:font_awesome_fonts']);
